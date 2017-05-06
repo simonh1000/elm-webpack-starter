eb0a147de1b6c603ddf271622afc0144107ff265
Simon Hampton <hotbelgo@gmail.com>
2017-05-06 16:19:13 +0200

    example has state and uses scss functionality

--- src/App.elm
+++ src/App.elm
@@ -1,19 +1,17 @@
 module App exposing (..)
 
-import Html exposing (Html, div, text, h1, ul, li, button)
+import Html exposing (Html, div, text, h1, p, button)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onClick)
-import Counter
 
 
 type alias Model =
-    { counter : Counter.Model
-    }
+    Int
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( { counter = Counter.init }, Cmd.none )
+    ( 0, Cmd.none )
 
 
 
@@ -21,23 +19,29 @@ init =
 
 
 type Msg
-    = CounterAction Counter.Msg
+    = Inc
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update message model =
     case message of
-        CounterAction counterAction ->
-            { model | counter = Counter.update counterAction model.counter } ! []
+        Inc ->
+            (model + 1) ! []
 
 
 
 -- VIEW
+-- view : Model -> Html Msg
 
 
-view : Model -> Html Msg
 view model =
     div [ class "container" ]
-        [ h1 [] [ text "Counter" ]
-        , Counter.view model.counter |> Html.map CounterAction
+        [ h1 [] [ text "Hot loading" ]
+        , p [] [ text "Click on the button below to increment the state. ccThen make a change to the source code and see how the updated state is retained after you recompile." ]
+        , p [] [ text <| toString model ]
+        , button
+            [ class "btn btn-primary"
+            , onClick Inc
+            ]
+            [ text "+ 1" ]
         ]
