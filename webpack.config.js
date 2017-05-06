a313123e8b5bf36a522308c136f61b8d7e3c14c1
Simon Hampton <hotbelgo@gmail.com>
2017-05-06 16:19:33 +0200

    update config to webpack2 syntax

--- webpack.config.js
+++ webpack.config.js
@@ -1,65 +1,84 @@
 const webpack = require('webpack');
-
-var elmSource = __dirname + '/src';
 var path = require('path');
 
+var elmSource = __dirname + '/src';
+
 module.exports = {
-  entry: './src/index.js',
+    entry: './src/index.js',
 
-  output: {
-    path: path.join(__dirname, "dist"),
-    filename: 'index.js'
-  },
+    output: {
+        path: path.join(__dirname, "dist"),
+        filename: 'index.js'
+    },
 
-  resolve: {
-      modules: [
-          path.join(__dirname, "src"),
-          "node_modules"
-      ],
-      extensions: ['.js', '.elm', '.scss']
-  },
-  plugins: [
-      new webpack.NoEmitOnErrorsPlugin()
-  ],
-  module: {
-    loaders: [
-      {
-        test: /\.html$/,
-        exclude: /node_modules/,
-        loader: 'file-loader?name=[name].[ext]'
-      },
-      {
-        test: /\.elm$/,
-        exclude: [/elm-stuff/, /node_modules/],
-        loader: 'elm-hot-loader!elm-webpack-loader?debug=true'
-        // loader: 'elm-hot!elm-webpack?cwd=' + elmSource
+    resolve: {
+        modules: [
+            path.join(__dirname, "src"),
+            "node_modules"
+        ],
+        extensions: ['.js', '.elm', '.scss']
     },
-    {
-        test: /\.scss$/,
-        exclude: [/elm-stuff/, /node_modules/],
-        loaders: ["style-loader", "css-loader", "sass-loader"]
-      },
-    {
-        test: /\.css$/,
-        exclude: [/elm-stuff/, /node_modules/],
-        loaders: ["style-loader", "css-loader"]
-    },
-    {
-        test: /\.(jpg|png|gif|svg|ico)$/,
-        exclude: [/elm-stuff/, /node_modules/],
-        loaders: ["url-loader"]
-      },
-            { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=10000&mimetype=application/font-woff" },
-            { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader" }
+    plugins: [
+        new webpack.NoEmitOnErrorsPlugin()
     ],
-    noParse: /\.elm$/
-  },
+    module: {
+        rules: [{
+                test: /\.html$/,
+                exclude: /node_modules/,
+                loader: 'file-loader?name=[name].[ext]'
+            },
+            {
+                test: /\.elm$/,
+                exclude: [/elm-stuff/, /node_modules/],
+                use: [{
+                        loader: "elm-hot-loader"
+                    },
+                    {
+                        loader: "elm-webpack-loader",
+                        options: {
+                            debug: true,
+                            warn: true
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.scss$/,
+                exclude: [/elm-stuff/, /node_modules/],
+                loaders: ["style-loader", "css-loader", "sass-loader"]
+            },
+            {
+                test: /\.css$/,
+                exclude: [/elm-stuff/, /node_modules/],
+                loaders: ["style-loader", "css-loader"]
+            },
+            {
+                test: /\.(jpg|png|gif|svg|ico)$/,
+                exclude: [/elm-stuff/, /node_modules/],
+                loaders: ["url-loader"]
+            },
+            {
+                test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                exclude: [/elm-stuff/, /node_modules/],
+                loader: "url-loader",
+                options: {
+                    limit: 10000,
+                    mimetype: "application/font-woff"
+                }
+            },
+            {
+                test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                exclude: [/elm-stuff/, /node_modules/],
+                loader: "file-loader"
+            }
+        ]
+    },
 
-  devServer: {
-    //   proxy: {
-    //       "/": "http://localhost:9779"
-    //   },
-    inline: true,
-    stats: 'errors-only'
-  }
+    devServer: {
+        //   proxy: {
+        //       "/": "http://localhost:9779"
+        //   },
+        inline: true,
+        stats: 'errors-only'
+    }
 };
