eac0a74e424017fac7afc77f1e759fb7ecb123ed
Simon Hampton <hotbelgo@gmail.com>
2017-05-06 08:32:20 +0200

    update libraries

--- src/App.elm
+++ src/App.elm
@@ -8,13 +8,12 @@ import Counter
 
 type alias Model =
     { counter : Counter.Model
-    , logs : List String
     }
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( { counter = Counter.init, logs = [] }, Cmd.none )
+    ( { counter = Counter.init }, Cmd.none )
 
 
 
@@ -22,22 +21,14 @@ init =
 
 
 type Msg
-    = NoOp
-    | CounterAction Counter.Msg
-    | AppendLog String
+    = CounterAction Counter.Msg
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update message model =
     case message of
-        NoOp ->
-            model ! []
-
         CounterAction counterAction ->
             { model | counter = Counter.update counterAction model.counter } ! []
-
-        AppendLog log ->
-            ( { model | logs = log :: model.logs }, Cmd.none )
 
 
 
