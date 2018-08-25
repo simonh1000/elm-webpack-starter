ddcddddaceab4d0735e17ff5ea8c55245a16657a
Simon Hampton <hotbelgo@gmail.com>
2018-08-25 18:33:17 +0200

    working!

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@
 
 require("./styles.scss");
 
-var app = require('./Main');
-app.Elm.Main.init({flags: 6});
+const {Elm} = require('./Main');
+var app = Elm.Main.init({flags: 6});
 
-app.Elm.Main.ports.toJs.subscribe(data => {
+app.ports.toJs.subscribe(data => {
     console.log(data);
 })
 // Use ES2015 syntax and let Babel compile it for you
