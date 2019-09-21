fa33f9a59a5acd7a2be6532ed32794034e7626d2
Simon Hampton <hotbelgo@gmail.com>
2019-09-21 08:18:56 +0200

    Minify (#27)
    * switch to google compiler
    
    * add markdown example
    
    * update deps
    
    * remove debug
    
    * bump

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 const webpack = require("webpack");
 const merge = require("webpack-merge");
-const elmMinify = require("elm-minify");
+const ClosurePlugin = require('closure-webpack-plugin');
 
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 const HTMLWebpackPlugin = require("html-webpack-plugin");
@@ -11,7 +11,7 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 var MODE =
     process.env.npm_lifecycle_event === "prod" ? "production" : "development";
-var withDebug = !process.env["npm_config_nodebug"];
+var withDebug = !process.env["npm_config_nodebug"] && MODE == "development";
 // this may help for Yarn users
 // var withDebug = !npmParams.includes("--nodebug");
 console.log('\x1b[36m%s\x1b[0m', `** elm-webpack-starter: mode "${MODE}", withDebug: ${withDebug}\n`);
@@ -125,7 +125,19 @@ if (MODE === "development") {
 }
 if (MODE === "production") {
     module.exports = merge(common, {
-        plugins: [
+        optimization: {
+            minimizer: [
+              new ClosurePlugin({mode: 'STANDARD'}, {
+                // compiler flags here
+                //
+                // for debugging help, try these:
+                //
+                // formatting: 'PRETTY_PRINT',
+                // debug: true
+                // renaming: false
+              })
+            ]
+          }, plugins: [
             // Delete everything from output-path (/dist) and report to user
             new CleanWebpackPlugin({
                 root: __dirname,
@@ -133,8 +145,6 @@ if (MODE === "production") {
                 verbose: true,
                 dry: false
             }),
-            // Minify elm code
-            new elmMinify.WebpackPlugin(),
             // Copy static assets
             new CopyWebpackPlugin([
                 {
