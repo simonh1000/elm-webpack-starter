16606bdd4d9fc9ad2205b913b7a70913ae5dee3a
Simon Hampton <hotbelgo@gmail.com>
2020-05-24 09:18:02 +0200

    minimise css assets (#33)
    * minimise css assets
    
    * Merge branch 'master' into css-minimise
    
    # Conflicts:
    #	package.json
    
    * bump
    
    * switch to npm for CI
    
    * switch to npm for CI

--- webpack.config.js
+++ webpack.config.js
@@ -2,19 +2,24 @@ const path = require("path");
 const webpack = require("webpack");
 const merge = require("webpack-merge");
 
-const ClosurePlugin = require('closure-webpack-plugin');
+const ClosurePlugin = require("closure-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 const HTMLWebpackPlugin = require("html-webpack-plugin");
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-// to extract the css as a separate file
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+// Production CSS assets - separate, minimised file
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 
 var MODE =
     process.env.npm_lifecycle_event === "prod" ? "production" : "development";
 var withDebug = !process.env["npm_config_nodebug"] && MODE == "development";
 // this may help for Yarn users
 // var withDebug = !npmParams.includes("--nodebug");
-console.log('\x1b[36m%s\x1b[0m', `** elm-webpack-starter: mode "${MODE}", withDebug: ${withDebug}\n`);
+console.log(
+    "\x1b[36m%s\x1b[0m",
+    `** elm-webpack-starter: mode "${MODE}", withDebug: ${withDebug}\n`
+);
 
 var common = {
     mode: MODE,
@@ -123,21 +128,27 @@ if (MODE === "development") {
         }
     });
 }
+
 if (MODE === "production") {
     module.exports = merge(common, {
         optimization: {
             minimizer: [
-              new ClosurePlugin({mode: 'STANDARD'}, {
-                // compiler flags here
-                //
-                // for debugging help, try these:
-                //
-                // formatting: 'PRETTY_PRINT',
-                // debug: true
-                // renaming: false
-              })
+                new ClosurePlugin(
+                    { mode: "STANDARD" },
+                    {
+                        // compiler flags here
+                        //
+                        // for debugging help, try these:
+                        //
+                        // formatting: 'PRETTY_PRINT',
+                        // debug: true
+                        // renaming: false
+                    }
+                ),
+                new OptimizeCSSAssetsPlugin({})
             ]
-          }, plugins: [
+        },
+        plugins: [
             // Delete everything from output-path (/dist) and report to user
             new CleanWebpackPlugin({
                 root: __dirname,
