af3a1b0c9383fea9152a495661163bbc84da4b3b
Simon Hampton <hotbelgo@gmail.com>
2020-12-25 09:02:31 +0100

    production build

--- webpack.config.js
+++ webpack.config.js
@@ -13,8 +13,7 @@ const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 var MODE =
     process.env.npm_lifecycle_event === "prod" ? "production" : "development";
 var withDebug = !process.env["npm_config_nodebug"] && MODE === "development";
-// this may help for Yarn users
-// var withDebug = !npmParams.includes("--nodebug");
+
 console.log(
     "\x1b[36m%s\x1b[0m",
     `** elm-webpack-starter: mode "${MODE}", withDebug: ${withDebug}\n`
@@ -27,7 +26,6 @@ var common = {
     output: {
         path: path.join(__dirname, "dist"),
         publicPath: "/",
-        // FIXME webpack -p automatically adds hash when building for production
         filename: MODE === "production" ? "[name]-[hash].js" : "index.js"
     },
     plugins: [
